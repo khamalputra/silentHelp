04d7d079be0bdc00d421c90c1167f161ac504e39
Ade Khamelia Putra <44743509+khamalputra@users.noreply.github.com>
2026-05-10 16:52:30 +0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'silenthelp-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js',
+  '/calculator.js',
+  '/sos-service.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
