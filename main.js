18a7856c37d9ec0c6c885f1f825a106a17b2bd36
Ade Khamelia Putra <44743509+khamalputra@users.noreply.github.com>
2026-05-10 16:57:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
   initializeSOSService();
   initCalculator();
 });
+
+// ======= PWA REGISTRATION =======
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered! ✅'))
+      .catch(err => console.log('Service Worker registration failed ❌', err));
+  });
+}
